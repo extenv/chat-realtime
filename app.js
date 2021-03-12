c8bd441b0c40a2579ff83c3344e0fe0a019bfc39
Ivan Maulana <im.ivan.maulana@gmail.com>
2021-03-11 20:33:17 -0800

    v.1.0

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Views/chat.html')
+})
+let UserOnline = 1
+io.on('connection', socket => {
+    socket.on('join', param => {
+       UserOnline++;
+        io.emit('UserOnline', UserOnline)
+    })
+    socket.on('message', param => {
+        io.emit('message', param)
+    })
+    socket.on('disconnect', param => {
+        UserOnline--;
+        io.emit('UserOnline',UserOnline)
+
+    })
+})
+server.listen(3000)
